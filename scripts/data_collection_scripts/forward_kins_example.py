fe0d3f56602d8ed8c7d503c8b525276b4c2d21f2
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-11-06 21:00:04 -0500

    Add forward kinematic example

---
+++ scripts/data_collection_scripts/forward_kins_example.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from typing import Dict, List
+from kincalib.Record.DataRecorder import DataReaderFromCSV, DataRecorder
+from kincalib.Calibration.HandEyeCalibration import Batch_Processing
+from kincalib.utils.Logger import Logger
+from kincalib.Motion.DvrkKin import DvrkPsmKin
+import json
+import numpy as np
+
+log = Logger(__name__).log
+
+def perform_hand_eye():
+    record_dict = DataRecorder.create_records()
+    file_path = "./data/experiments/repetabability_experiment_rosbag01/01-11-2023-20-24-30/record_001.csv"
+    # file_path = "./data/experiments/repetabability_experiment_rosbag01/01-11-2023-20-28-58/record_001.csv"
+    # file_path = "./data/experiments/repetabability_experiment_rosbag01/01-11-2023-20-33-24/record_001.csv"
+
+    file_path = Path(file_path)
+    assert file_path.exists(), "File does not exist"
+    log.info(f"Analyzing experiment {file_path.parent.name}")
+
+    data_dict = DataReaderFromCSV(file_path, record_dict).data_dict
+
+    measured_jp = data_dict["measured_jp"]
+    measured_cp = data_dict["measured_cp"]
+    psm_kin = DvrkPsmKin()
+    calculated_cp = psm_kin.fkine(measured_jp)
+
+    idx = 50 
+    log.info("measured from robot")
+    log.info(measured_cp[:,:,idx])
+    log.info("calculated with python fkine model")
+    log.info(calculated_cp[idx].data[0])
+    log.info("error")
+    log.info(measured_cp[:,:,idx] - calculated_cp[idx].data[0])
+
+
+if __name__ == "__main__":
+    perform_hand_eye()
