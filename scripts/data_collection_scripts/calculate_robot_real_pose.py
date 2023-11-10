7217003f952611ba1aee6e6ab5aa381bb163ceee
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-11-10 16:27:40 -0500

    Remove unused methods
    - pos and orientation error are calculated somewhere else

--- scripts/data_collection_scripts/calculate_robot_real_pose.py
+++ scripts/data_collection_scripts/calculate_robot_real_pose.py
@@ -74,20 +74,6 @@ class ExperimentalData:
         
         return T_RG_actual
 
-    @classmethod
-    def calculate_position_error(cls, T_RG:np.ndarray, T_RG_actual:np.ndarray):
-        return np.linalg.norm((T_RG_actual - T_RG)[:3,3,:],axis=0)*1000
-
-    @classmethod
-    def calculate_orientation_error(cls, T_RG:np.ndarray, T_RG_actual:np.ndarray):
-        orientation_error = []
-        for i in range(T_RG.shape[ 2 ]):
-            T_RG_ith = Rotation3D(Rotation3D.trnorm(T_RG[:3,:3,i]))
-            T_RG_actual_ith = Rotation3D(Rotation3D.trnorm(T_RG_actual[:3,:3, i]))
-            axis_angle = (T_RG_ith.T @ T_RG_actual_ith).as_rotvec()
-            orientation_error.append( np.linalg.norm(axis_angle))
-        
-        return np.array(orientation_error) * 180/np.pi
 
 
 
