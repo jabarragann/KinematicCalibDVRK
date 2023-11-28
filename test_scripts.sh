f86fe87530a2f0798498e21ab82b2693c449841f
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-11-27 21:39:27 -0500

    Change test script file

--- test_scripts.sh
+++ test_scripts.sh
@@ -11,9 +11,9 @@ set -x
 # --data_file data/experiments/data_collection2_updated_collection_scripts/13-11-2023-20-33-49/combined_data.csv \
 # --handeye_file ./data/experiments/data_collection2_updated_collection_scripts/13-11-2023-20-33-49/hand_eye_calib.json
 
-python scripts/data_collection_scripts/calculate_robot_real_pose.py  \
---data_file data/experiments/data_collection3_orig/14-11-2023-18-37-32/record_001.csv \
---handeye_file ./data/experiments/data_collection3/combined/hand_eye_calib.json
+# python scripts/data_collection_scripts/calculate_robot_real_pose.py  \
+# --data_file data/experiments/data_collection3_orig/14-11-2023-18-37-32/record_001.csv \
+# --handeye_file ./data/experiments/data_collection3/combined/hand_eye_calib.json
 
 
 # python scripts/data_collection_scripts/calculate_robot_real_pose.py \
@@ -28,3 +28,13 @@ python scripts/data_collection_scripts/calculate_robot_real_pose.py  \
 # --handeye_file ./data/experiments/data_collection3/combined/hand_eye_calib.json \
 # --data_file ./data/experiments/data_collection3/combined/record_001_2.csv \
 # --out_name dataset2.csv
+ 
+
+#---------------------------------------------------------------
+# Generate dataset from collected data 03
+#---------------------------------------------------------------
+
+python scripts/data_collection_scripts/calculate_robot_real_pose.py  \
+--data_file ./data/experiments/data_collection3/soft_traj4/record_001.csv \
+--handeye_file ./data/experiments/data_collection3/combined/hand_eye_calib.json \
+--out_name  dataset4.csv
